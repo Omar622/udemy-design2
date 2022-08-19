f13d3c47c0b8d3e9f1167e98960d687226e7f189
unknown <omaramin622@gmail.com>
2022-08-19 23:40:32 +0200

    reformating code

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,17 +1,20 @@
 import React from "react";
 import Rate from "./Rate.js";
+import data from "../dummy_data.js"
 
 function Card(props) {
-    // props: id image title author rating people price bestSeller
+    // props: field, id 
+    let item = data[props.field].find(element => parseInt(element["id"]) === parseInt(props.id));
+
     return <article className="courses-grid-item">
-        <img src={props.image} alt="python" height="150px" width="260px"></img>
+        <img src={item["image"]} alt="python" height="150px" width="260px"></img>
         <header style={{padding_left: "12px"}}>
-            <h3 style={{margin:"5px"}}>{props.title.length > 41 ? (props.title.substring(0, 40) + "...") : props.title}</h3>
-            <p className="coures-grid-item-description" style={{color:"#876f89"}}>{props.author}</p>
-            <p style={{display: "inline", margin: "0px 5px 0px 5px", font_size: "12px", color:"#ce810e"}}>{props.rating}</p>
-            <Rate rate={props.rating}></Rate>
-            <p style={{display: "inline", font_size: "12px", color: "#738abb"}}>({props.people})</p>
-            <h3 style={{margin: "0px 5px"}}>E€ {props.price}</h3>
+            <h3 style={{margin:"5px"}}>{item["title"].length > 41 ? (item["title"].substring(0, 40) + "...") : item["title"]}</h3>
+            <p className="coures-grid-item-description" style={{color:"#876f89"}}>{item["author"]}</p>
+            <p style={{display: "inline", margin: "0px 5px 0px 5px", font_size: "12px", color:"#ce810e"}}>{item["rating"]}</p>
+            <Rate rate={item["rating"]}></Rate>
+            <p style={{display: "inline", font_size: "12pitem", color: "#738abb"}}>({item["people"]})</p>
+            <h3 style={{margin: "0px 5px"}}>E€ {item["price"]}</h3>
         </header>
     </article>
 }
